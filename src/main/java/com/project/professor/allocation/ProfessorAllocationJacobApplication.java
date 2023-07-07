554507ea424271a5561af36f63ebc1fc9047a282
Jacob Berenstein Neto <jbnlivriaria@gmail.com>
2023-07-07 20:21:12 -0300

    add project

---
+++ src/main/java/com/project/professor/allocation/ProfessorAllocationJacobApplication.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationJacobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationJacobApplication.class, args);
+	}
+
+}
